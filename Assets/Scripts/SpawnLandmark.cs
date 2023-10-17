7311db7478518263fd9916d5d483668c9739d72b
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-10-17 20:31:07 +0100

    Sofa Imported as well as Terrible Katana Model

---
+++ Assets/Scripts/SpawnLandmark.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLandmark : MonoBehaviour
+{
+
+    public Transform self;
+    public GameObject[] my_spawnables;
+    public int chosen_landmark;
+
+    public SpawnLandmark my_other_spawner_a;
+    public SpawnLandmark my_other_spawner_b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
