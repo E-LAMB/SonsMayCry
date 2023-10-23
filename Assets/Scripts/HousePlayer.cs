35b478645ec122d3944c3c8cf90a8116150c7b8f
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-10-23 19:26:49 +0100

    Selector Works

--- Assets/Scripts/HousePlayer.cs
+++ Assets/Scripts/HousePlayer.cs
@@ -149,6 +149,12 @@ public class HousePlayer : MonoBehaviour
                 Vector3 wishDirection = (forward * axis.x + my_camera.transform.right * axis.y + Vector3.up * rb.velocity.y);
                 rb.velocity = wishDirection;
             }
+        } else
+        {
+                Vector2 axis = new Vector2(0f, 0f) * speed;
+                Vector3 forward = new Vector3(-my_camera.transform.right.z, 0, my_camera.transform.right.x);
+                Vector3 wishDirection = (forward * axis.x + my_camera.transform.right * axis.y + Vector3.up * rb.velocity.y);
+                rb.velocity = wishDirection;
         }
     }
 }
--- Assets/Scripts/PowerSelector.cs
+++ Assets/Scripts/PowerSelector.cs
@@ -1,27 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerSelector : MonoBehaviour
 {
 
     public GameObject my_menu;
-    public HousePl
+    public HousePlayer player;
+    public HouseInteractor player_int;
 
-    public void ChangeState()
+    public int selected_slot;
+
+    public int power_1;
+    public int power_2;
+
+    public Sprite[] all_sprites;
+
+    public Image power_1_image;
+    public Image power_2_image;
+
+    public GameObject outline_PS1;
+    public GameObject outline_PS2;
+
+    public bool[] unlocked_abilities;
+
+    public void OpenMenu()
     {
+        bool new_state = true;
 
+        Debug.Log("Log1");
+
+        my_menu.SetActive(new_state);
+        player.enabled = !new_state;
+        player_int.enabled = !new_state;
+
+        if (new_state)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        } else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+    }
+
+    public void CloseMenu()
+    {
+        bool new_state = false;
+
+        Debug.Log("Log2");
+
+        my_menu.SetActive(new_state);
+        player.enabled = !new_state;
+        player_int.enabled = !new_state;
+
+        if (new_state)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        } else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+    }
+
+    public void ChangeSelectedSlot(int new_slot)
+    {
+        selected_slot = new_slot;
+        outline_PS1.SetActive(new_slot == 1);
+        outline_PS2.SetActive(new_slot == 2);
+    }
+
+    public void PlaceIntoSlot(int to_place)
+    {
+        if (unlocked_abilities[to_place])
+        {
+            if (selected_slot == 1)
+            {
+                power_1 = to_place;
+            }
+            if (selected_slot == 2)
+            {
+                power_2 = to_place;
+            }
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CloseMenu();
+        selected_slot = 1;
+        outline_PS1.SetActive(true);
+        outline_PS2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        power_1_image.sprite = all_sprites[power_1];
+        power_2_image.sprite = all_sprites[power_2];
     }
 }
